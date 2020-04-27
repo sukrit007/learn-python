2b275100da9271dabeb9aebe02d504e259d12226
sukrit007 <skhera@intraedge.com>
2020-04-26 22:15:15 -0700

    flake8 and autopep8 class inheritance

--- README.md
+++ README.md
@@ -1,5 +1,54 @@
 # Learn Python
 
+## Setup
+### Python
+For Mac, install python 3.8 using:
+```bash
+brew install python@3.8
+```
+
+### Virtualenv
+- Install virtualenv
+```bash
+pip3 install virtualenv --user
+```
+- Setup virtual environment for the project
+```bash
+virtualenv -p /usr/local/opt/python@3.8/bin/python3  venv
+```
+
+- Activate virtualenv
+```bash
+source venv/bin/activate
+```
+
+### Install Dependencies
+```bash
+echo 'Installing dev dependencies...'
+pip install -r requirements-dev.txt
+
+echo 'Installing test dependencies...'
+pip install -r tests/requirements.txt
+
+echo 'Installing test dependencies...'
+pip install -r basics/requirements.txt
+```
+
+
+## Code Style
+### Linting
+In order to check for conventions using flake8 run command:
+```bash
+flake8 .
+```
+The flake8 configuration can be found in [setup.cfg](./setup.cfg) in section flake8.
+
+
+### Fix 
+```bash
+autopep8 --aggressive -r --in-place .
+```
+
 ## Tools List
 https://github.com/vintasoftware/python-linters-and-code-analysis
 
---
+++ basics/funclasses.py
@@ -0,0 +1,43 @@
+class ClassA:
+    count = 10
+
+    def __init__(self, size=10):
+        self.size = size
+
+    def __repr__(self) -> str:
+        return 'ClassA(count=%d,size=%d)' % (self.count, self.size)
+
+    def __str__(self) -> str:
+        return 'count=%d,size=%d' % (self.count, self.size)
+
+
+class ClassB(ClassA):
+    bcount = 20
+
+    def __init__(self, size=10, bsize=100):
+        super().__init__(size)
+        self.bsize = bsize
+
+    def __repr__(self) -> str:
+        return "ClassB(bsize=%d)" % self.bsize + "--" + super().__repr__()
+
+    def __str__(self):
+        return "bsize=%d" % self.bsize + "," + super().__str__()
+
+
+if __name__ == '__main__':
+    print("ClassA#count: %d" % ClassA.count)
+    obj1 = ClassA()
+    obj2 = ClassA(20)
+
+    print("obj1#count(original): %d" % obj1.count)
+    obj1.count = 100
+    print("obj1#count(modified): %d" % obj1.count)
+    print("obj2#count: %d" % obj2.count)
+
+    print("Obj1: %r" % obj1)
+    print("Obj2.__dict__: %r" % obj2.__dict__)
+
+    objB = ClassB(100)
+    print("ObjB(repr): %r" % objB)
+    print("ObjB(str): %s" % objB)
